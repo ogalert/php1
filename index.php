80bead044331daea8692b3c9363f7dd6c0c75340
ogalert <130741989+ogalert@users.noreply.github.com>
2023-04-14 10:49:11 +0200

    Update and rename phptest-temat4.php to index.php

--- index.php
+++ index.php
@@ -179,4 +179,30 @@ switch ($miesiac) {
     default: echo "Błedny numer miesiąca";
     break;
 }
+//zadanie z tematu 5.
+for ($x = 1; $x <= 10; $x++) {
+  if ($x ** 2) echo $x, "<br>";
+}
+//zadanie z sprawdzianu
+#zmienne, czyli imie, nazwisko oraz wiek
+$imie = "Oliwier";
+$nazwisko = "Galert";
+$wiek = 17;
+
+#skrypt który sprawdza wiek i umieszcza dany napis dla danego wieku
+if ($wiek >= 18 && $wiek < 21) {
+    echo $imie, " ", $nazwisko, " Możesz tylko głosować.";
+}
+else if ($wiek >= 21 && $wiek < 30) {
+    echo $imie, " ", $nazwisko, " Możesz głosować i kandydować do Sejmu.";
+}
+else if ($wiek >= 30 && $wiek < 35) {
+    echo $imie, " ", $nazwisko, " Możesz głosować i kandydować do Sejmu i Senatu.";
+}
+else if ($wiek >=35) {
+    echo $imie, " ", $nazwisko, " Możesz głosować i kandydować do Sejmu i Senatu oraz na Prezydenta.";
+}
+else {
+    echo $imie, " ", $nazwisko, " Nie możesz jeszcze głosować.";
+}
 ?>
